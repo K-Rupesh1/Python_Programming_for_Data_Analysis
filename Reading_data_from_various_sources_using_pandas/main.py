5c5f352362a40152e322f1cce6b1f22a5e914bcd
K RUPESH <KurakulaRupesh1234@outlook.com>
2026-08-25 12:01:39 +0530

    Add files via upload

---
+++ Reading_data_from_various_sources_using_pandas/main.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from io import StringIO
+data='[{"name":"Rupesh","email":"kurakularupesh1234@gmail.com","job profile":"software developer"}]'
+df=pd.read_json(StringIO(data))
+print(df)
+print()
+#convert dataframe into json
+#df=df.to_json(orient='index')
+#print(df)
+df=df.to_json(orient='records')
+print(df)
+
+df=pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data",header=None)
+print(df.head(5))
+#to save as csv file
+df.to_csv("wine.csv")
+
+url="https://www.fdic.gov/bank-failures/failed-bank-list"
+df=pd.read_html(url)[0]
+print(df)
+
+df=pd.read_csv("C:/Users/Kurak/Downloads/Data_ANalysis_Using_Python/Reading_data_from_various_sources_using_pandas/wine.csv")
+# convert the csv data as a excel data
+excel=df.to_excel("sample.xlsx")
